952ccd0f8ef4b1475c52ae0ef1e21f595052fc41
Cenk Beri <cenkberi@gmail.com>
2022-01-26 16:25:26 +0300

    ok

--- js/main.js
+++ js/main.js
@@ -1,38 +1,189 @@
-const button = document.querySelector('#button');
-const result = document.getElementById('result');
-const database = [];
+let box1   = document.querySelector('.box1');
+let box2   = document.querySelector('.box2');
+let box3   = document.querySelector('.box3');
+let box4   = document.querySelector('.box4');
+let boxrow = document.querySelector('.boxrow');
 
-button.addEventListener("click", () => {
+const sleft    = document.querySelector('#sleft');
+const sright   = document.querySelector('#sright');
+const bdelete  = document.querySelector('#bdelete');
 
-    const name = document.querySelector('#name').value;
-    const job  = document.querySelector('#job').value;
-    const age  = document.querySelector('#age').value;
+box1.addEventListener("click" , () =>{
 
-    class Player {
-        constructor(name, job, birth) {
-            this.name = name;
-            this.job = job;
-            this.age = age;
-        }
+   if(box1.style.opacity === "1")
+   {
+      box2.style.border = "5px solid #000000";
+      box2.style.opacity = "1";
+      box3.style.border = "5px solid #000000";
+      box3.style.opacity = "1";
+      box4.style.border = "5px solid #000000";
+      box4.style.opacity = "1";
+      box1.style.border = "5px solid red";
+      box1.style.opacity = "0.6";
 
-        generalInfo() {
-            return `${this.name} working at ${this.job} and ${this.age} years old.`
-        }
 
-        ageInfo() {
-            const age = new Date().getFullYear() - this.age;
-            return ` His birth year is ${age}`
-        }
-    }
+      sleft.addEventListener("click", () =>
+      {
+         if(box1.style.opacity === "0.6"){
 
-    const player1 = new Player(name, job, age);
-    const data1 = player1.generalInfo();
-    const data2 = player1.ageInfo();
-    database.push(player1);
+          boxrow.prepend(box1);
+         }
+      });
 
-    if(player1.age < 30)
-    {
-        result.innerHTML += `<p>${data1}${data2}<p>`;
-    }
-    console.table(database);
+      sright.addEventListener("click", () =>
+      {
+         if(box1.style.opacity === "0.6"){
+          boxrow.appendChild(box1);
+         }
+      });
+      bdelete.addEventListener("click", () =>
+      {
+         if(box1.style.opacity === "0.6"){
+          boxrow.removeChild(box1);
+         }
+      });
+
+   }
+
+   else
+   {
+      box1.style.border = "5px solid #000000";
+      box1.style.opacity = "1";
+   }
+
+});
+
+box2.addEventListener("click" , () =>{
+
+   if(box2.style.opacity === "1")
+   {
+      box1.style.border = "5px solid #000000";
+      box1.style.opacity = "1";
+      box3.style.border = "5px solid #000000";
+      box3.style.opacity = "1";
+      box4.style.border = "5px solid #000000";
+      box4.style.opacity = "1";
+      box2.style.border = "5px solid red";
+      box2.style.opacity = "0.6";
+
+
+         sleft.addEventListener("click", () =>
+      {
+         if(box2.style.opacity === "0.6"){
+
+          boxrow.prepend(box2);
+         }
+      });
+
+      sright.addEventListener("click", () =>
+      {
+         if(box2.style.opacity === "0.6"){
+          boxrow.appendChild(box2);
+         }
+      });
+      bdelete.addEventListener("click", () =>
+      {
+         if(box2.style.opacity === "0.6"){
+          boxrow.removeChild(box2);
+         }
+      });
+
+   }
+
+   else
+   {
+      box2.style.border = "5px solid #000000";
+      box2.style.opacity = "1";
+   }
+
+});
+
+box3.addEventListener("click" , () =>{
+
+   if(box3.style.opacity === "1")
+   {
+      box1.style.border = "5px solid #000000";
+      box1.style.opacity = "1";
+      box2.style.border = "5px solid #000000";
+      box2.style.opacity = "1";
+      box4.style.border = "5px solid #000000";
+      box4.style.opacity = "1";
+      box3.style.border = "5px solid red";
+      box3.style.opacity = "0.6";
+
+
+         sleft.addEventListener("click", () =>
+      {
+         if(box3.style.opacity === "0.6"){
+
+          boxrow.prepend(box3);
+         }
+      });
+
+      sright.addEventListener("click", () =>
+      {
+         if(box3.style.opacity === "0.6"){
+          boxrow.appendChild(box3);
+         }
+      });
+      bdelete.addEventListener("click", () =>
+      {
+         if(box3.style.opacity === "0.6"){
+          boxrow.removeChild(box3);
+         }
+      });
+
+   }
+
+   else
+   {
+      box3.style.border = "5px solid #000000";
+      box3.style.opacity = "1";
+   }
+
+});
+
+box4.addEventListener("click" , () =>{
+
+   if(box4.style.opacity === "1")
+   {
+      box1.style.border = "5px solid #000000";
+      box1.style.opacity = "1";
+      box2.style.border = "5px solid #000000";
+      box2.style.opacity = "1";
+      box3.style.border = "5px solid #000000";
+      box3.style.opacity = "1";
+      box4.style.border = "5px solid red";
+      box4.style.opacity = "0.6";
+
+
+         sleft.addEventListener("click", () =>
+      {
+         if(box4.style.opacity === "0.6"){
+
+          boxrow.prepend(box4);
+         }
+      });
+
+      sright.addEventListener("click", () =>
+      {
+         if(box4.style.opacity === "0.6"){
+          boxrow.appendChild(box4);
+         }
+      });
+      bdelete.addEventListener("click", () =>
+      {
+         if(box4.style.opacity === "0.6"){
+          boxrow.removeChild(box4);
+         }
+      });
+
+   }
+
+   else
+   {
+      box4.style.border = "5px solid #000000";
+      box4.style.opacity = "1";
+   }
+
 });
