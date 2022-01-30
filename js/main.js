59a142887d3dd0330f767e690021be7f865b4fe5
Cenk Beri <cenkberi@gmail.com>
2022-01-30 05:17:22 +0300

    ok

--- js/main.js
+++ js/main.js
@@ -1,57 +1,63 @@
-const firmName  = "Karaca";
-const firmPlace = "Şişli";
-const firmYear  = 1996;
+const mybut = document.querySelector('.mybut');
+const screen = document.querySelector('.screen');
+let numb = 1;
 
-class Company{
-    constructor(_name,_place,_year){
-        this.name  = _name;
-        this.place = _place;
-        this.year  = _year;
-    }
-    get CompName(){
-        return this.name;
-    }
-    get CompPlace(){
-        return this.place;
-    }
-    get CompYear(){
-        return this.year;
-    }
+mybut.addEventListener('click', () => {
+    for (let x = 1; x <= 50; x++) {
+        const letters = [0, 1, "A", 2, 3, "B", 4, 5, "C", 6, 7, "D", 8, 9, "F"];
+        let result = [];
 
-    set CompName(newName){
-        this.name = newName;
-    }
-    set CompPlace(newPlace){
-        this.place = newPlace;
-    }
-    set CompYear(newYear){
-        this.year = newYear;
-    }
-    get personeldatas()
-     {
-        return `${this.empName}, ${this.empPos} pozisyonunda ${this.name} firmasında çalışıyordur.${this.name},${this.year} yılında kurulmuştur.Merkezi ${this.place} ilçesindedir...`;
-     }
-}
+        for (let i = 0; i <= 5; i++) {
+            let ranNum = Math.floor((Math.random() * 15));
+            result.push(letters[ranNum]);
+        }
 
-class employee extends Company{
+        result.unshift("#");
+        let sum = result.join('');
 
-     constructor(_name,_place,_year,_empName,_empPos,_empAge)
-     {
-        super(_name,_place,_year);
-        this.empName = _empName;
-        this.empPos  = _empPos;
-        this.empAge  = _empAge;
-     }
-}
 
-const emp1 = new employee(firmName,firmPlace,firmYear,"Cenk Beri","Mağaza Müdürü",33);
-const emp2 = new employee(firmName,firmPlace,firmYear,"Yeliz Çağlayan","Satış Danışmanı",22);
-const emp3 = new employee(firmName,firmPlace,firmYear,"Doğukan Noberi","Part Star",25);
 
-console.log(emp1.personeldatas);
-console.log(emp2.personeldatas);
-console.log(emp3.personeldatas);
+        numb = numb + 0.5;
+        let box = document.createElement("div");
+        box.style.backgroundColor = `${sum}`;
+        box.style.left = "0";
+        box.style.top = "0";
+        box.style.position = "absolute";
+        box.style.height = "10rem";
+        box.style.width = "10rem";
+        box.style.border = "2px solid #000000";
+        box.style.animationName = "move";
+        box.style.animationDelay = `${numb}s`;
+        box.style.animationDuration = "5s";
+        box.style.animationIterationCount = "infinite";
+        box.style.animationTimingFunction = "linear";
+        box.style.borderRadius = "50%";
+        box.style.display = "flex";
+        box.style.alignItems = "center";
+        box.style.justifyContent = "center";
+        box.style.fontSize = "2rem";
+        box.style.transform = "scale(0.2)";
+        screen.appendChild(box);
 
-if(emp1.CompName === firmName){
-    document.write(`${emp1.personeldatas}`);
-}
+        let box2 = document.createElement("div");
+        box2.style.backgroundColor = `${sum}`;
+        box2.style.left = "4rem";
+        box2.style.top = "4rem";
+        box2.style.position = "absolute";
+        box2.style.height = "10rem";
+        box2.style.width = "10rem";
+        box2.style.border = "2px solid #000000";
+        box2.style.animationName = "dance";
+        box2.style.animationDelay = `${numb}s`;
+        box2.style.animationDuration = "5s";
+        box2.style.animationIterationCount = "infinite";
+        box2.style.animationTimingFunction = "linear";
+        box2.style.borderRadius = "50%";
+        box2.style.display = "flex";
+        box2.style.alignItems = "center";
+        box2.style.justifyContent = "center";
+        box2.style.fontSize = "2rem";
+        box2.style.transform = "scale(0.2)";
+        screen.appendChild(box2);
+    };
+});
